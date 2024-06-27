ba06d5d1fb4539c4092a7341bbaab1ffada87ac6
Christopher Wittlinger <c.wittlinger@stainly.com>
2024-06-27 21:54:27 +0200

    added poetry

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,7 @@
+-- Load the base configuration
 require 'config.configurations'
 
+-- Setup lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
