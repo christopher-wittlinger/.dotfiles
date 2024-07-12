975036e650445346641242a7c8afabff245ba3a7
Christopher Wittlinger <c.wittlinger@stainly.com>
2024-07-12 15:12:57 +0200

    adjusted nvim oil to also show hidden files

--- nvim/.config/nvim/lua/plugins/files.lua
+++ nvim/.config/nvim/lua/plugins/files.lua
@@ -3,7 +3,11 @@ return {
   opts = {},
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function(_, opts)
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open directory' })
   end,
 }
